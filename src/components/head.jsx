9ea93fb25291995d16934430662abdc21dddc750
TRBhavaniShankar <tr.bhavanishankar93@gmail.com>
2021-06-19 07:29:26 -0230

    setting main components

---
+++ src/components/head.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Head extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            
+        </div> );
+    }
+}
+ 
+export default Head;
